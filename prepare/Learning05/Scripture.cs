8924c2b909752f33e907db590f3cebfed7a3bec5
moudoudou noe <moudoudounoe@gmail.com>
2024-11-16 18:08:07 +0100

    W3 prepare

---
+++ prepare/Learning05/Scripture.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = text.Split(' ').Select(word => new Word(word)).ToList();
+    }
+
+    public void HideRandomWords(int count)
+    {
+        var visibleWords = _words.Where(word => !word.IsHidden()).ToList();
+        var random = new Random();
+
+        for (int i = 0; i < count && visibleWords.Count > 0; i++)
+        {
+            int index = random.Next(visibleWords.Count);
+            visibleWords[index].Hide();
+            visibleWords.RemoveAt(index);
+        }
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        return _words.All(word => word.IsHidden());
+    }
+
+    public override string ToString()
+    {
+        return $"{_reference}\n{string.Join(" ", _words)}";
+    }
+}
